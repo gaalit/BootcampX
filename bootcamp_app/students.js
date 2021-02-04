01da40991149b0807f83a193285c2f225549c8e7
Galit <galit.gerasimov3@gmail.com>
2021-02-04 01:22:04 +0000

    Parameterized Query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,24 +7,24 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-// pool.query(`
-// SELECT id, name, cohort_id
-// FROM students
-// LIMIT 5;
-// `)
-// .then(res => {
-//   console.log(res.rows);
-// })
-// .catch(err => console.error('query error', err.stack));
+//1. create a querry replacing all incoming data as $(number=> starting at 1)
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
+//2. for each $(data) create variables
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+//3. store the vulnerable data into one array
+const values = [`%${cohortName}%`, limit];
+
+
+//4. join querry and array of variables in the pool.querry
+pool.query(queryString, values).then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const querryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
+`
+const cohortName = process.argv[2] || 'JUL02';
+const values = [process.argv[2] || 'JUL02'] ;
+
+pool.query(querryString, values).then(data => {
+  data.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
-});
+})
